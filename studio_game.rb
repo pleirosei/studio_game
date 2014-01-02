c64d5e6080397aad1f710cf978fbd2225607baaa
Sean Livingston <sean.d.livingston@gmail.com>
2014-01-02 08:27:01 -0500

    Now using Classes.

--- studio_game.rb
+++ studio_game.rb
@@ -1,13 +1,38 @@
-def time
-	current_time = Time.new
-	current_time.strftime("%I:%M:%S")
+class Player
+	
+	def initialize(name, health=100)
+		@name = name.capitalize
+		@health = health
+	end
+
+	def to_s
+		"I'm #{@name} with a health of #{@health}."
+	end
+
+	def blam
+		@health -= 10
+		"#{@name} got blammed!"
+	end
+
+	def w00t
+		@health += 15
+		"#{@name} got w00ted!"
+	end
+
 end
 
-def say_hello(name, health=100)
-	"I'm #{name.capitalize} with a health of #{health} as of #{time}."
-end
 
-puts say_hello("larRy", 60)
-puts say_hello("CURLY", 125)
-puts say_hello("moE")
-puts say_hello("sheMp", 90)
+
+player1 = Player.new("moe")
+puts player1
+
+player2 = Player.new("larry", 60)
+puts player2
+
+player3 = Player.new("curly", 125)
+puts player3
+
+puts player3.blam
+puts player3
+puts player3.w00t
+puts player3
