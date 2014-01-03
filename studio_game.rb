dfd0ce6ab70533f015ecae720436a02fde1fc5a4
Sean Livingston <sean.d.livingston@gmail.com>
2014-01-02 23:31:59 -0500

    Added Attribute Functionality.

--- studio_game.rb
+++ studio_game.rb
@@ -1,12 +1,14 @@
 class Player
-	
+	attr_accessor :name
+	attr_reader :health
+
 	def initialize(name, health=100)
 		@name = name.capitalize
 		@health = health
 	end
 
-	def to_s
-		"I'm #{@name} with a health of #{@health}."
+	def name=(new_name)
+		@name = new_name.capitalize
 	end
 
 	def blam
@@ -19,20 +21,19 @@ class Player
 		"#{@name} got w00ted!"
 	end
 
+	def score
+		@health + @name.length
+	end
+
+	def to_s
+		"I'm #{@name} with a health of #{score}."
+	end
 end
 
-
-
-player1 = Player.new("moe")
-puts player1
-
 player2 = Player.new("larry", 60)
+puts player2.name
+player2.name = "lawrence"
+puts player2.name
+puts player2.health
+puts player2.score
 puts player2
-
-player3 = Player.new("curly", 125)
-puts player3
-
-puts player3.blam
-puts player3
-puts player3.w00t
-puts player3
