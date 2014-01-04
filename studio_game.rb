988606106eec0c2dd7f5ad6c4792c2357491c6ff
Sean Livingston <sean.d.livingston@gmail.com>
2014-01-04 13:38:25 -0500

    Working with Arrays.

--- studio_game.rb
+++ studio_game.rb
@@ -26,14 +26,25 @@ class Player
 	end
 
 	def to_s
-		"I'm #{@name} with a health of #{score}."
+		"I'm #{@name} with a health of #{@health} and a score of #{score}."
 	end
+
 end
 
+
+player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
-puts player2.name
-player2.name = "lawrence"
-puts player2.name
-puts player2.health
-puts player2.score
-puts player2
+player3 = Player.new("curly", 125)
+player4 = Player.new("shemp", 90)
+
+
+players = [player1, player2, player3]
+players << player4
+players.delete_at(2)
+
+players.each do |player|
+	puts player.blam
+	puts player.w00t
+	puts player.w00t
+	puts player
+end
